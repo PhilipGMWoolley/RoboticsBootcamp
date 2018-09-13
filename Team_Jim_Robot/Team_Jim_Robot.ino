efd40fbb3fb8e9d7fa14552f1377f681c904e2d0
waqas <wzquresh>
2018-09-13 17:31:00 -0400

    fixed sonar issue, added code outline

--- Team_Jim_Robot/Team_Jim_Robot.ino
+++ Team_Jim_Robot/Team_Jim_Robot.ino
@@ -9,6 +9,9 @@ const int LEFT_ECHO = 7;
 int Wall_Distance;
 int Front_Distance;
 
+void FirstObstacleArch();
+void SecondObstacleArch();
+
 void setup() {
   Serial.begin(9600);
   pinMode(FRONT_TRIG, OUTPUT);
@@ -20,16 +23,35 @@ void setup() {
 void loop() {
   digitalWrite(LEFT_TRIG, HIGH);
   delayMicroseconds(10);
+  digitalWrite(LEFT_TRIG, LOW);
   int wall_duration = pulseIn(LEFT_ECHO, HIGH);
   Wall_Distance = ((wall_duration/29)/2);
   
   digitalWrite(FRONT_TRIG, HIGH);
   delayMicroseconds(10);
+  digitalWrite(FRONT_TRIG, LOW);
   int front_duration = pulseIn(FRONT_ECHO, HIGH);
-  Serial.println(front_duration);
   Front_Distance = ((front_duration/29)/2);
 
   Serial.print("FRONT DISTANCE: ");
   Serial.println(Front_Distance);
+  Serial.print("WALL DISTANCE: ");
+  Serial.println(Wall_Distance);
+
+  //If wall is more than MAX_WALL_DISTANCE move closer to wall
+  //Else if wall si less than MIN_WALL_DISTANCE move farther from wall
+
+  //If obstacle is approaching
+    //If First Obstacle do FirstObstacleArch
+    //Else do SecondObstacleArch  
   
 }
+
+void FirstObstacleArch(){
+  
+}
+
+void SecondObstacleArch(){
+  
+}
+
