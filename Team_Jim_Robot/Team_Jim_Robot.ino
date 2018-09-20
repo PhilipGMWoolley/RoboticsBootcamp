785dfb6a3e6ef10a2dd2fd01fbb1a869c6bbad13
waqas <wzquresh>
2018-09-20 15:23:04 -0400

    added major logic blocks and comments

--- Team_Jim_Robot/Team_Jim_Robot.ino
+++ Team_Jim_Robot/Team_Jim_Robot.ino
@@ -1,16 +1,25 @@
+#include <Adafruit_MotorShield.h>
+
 int MIN_DIST_TO_WALL = 5;
 int MAX_DIST_TO_WALL = 10;
+int MAX_FRONT_DIST = 15;
 
-const int FRONT_TRIG = 4;
-const int FRONT_ECHO = 5;
-const int LEFT_TRIG = 6;
-const int LEFT_ECHO = 7;
+const int FRONT_TRIG = 8;
+const int FRONT_ECHO = 9;
+const int LEFT_TRIG = 11;
+const int LEFT_ECHO = 10;
 
 int Wall_Distance;
 int Front_Distance;
+bool FIRST_OBSTACLE;
 
 void FirstObstacleArch();
 void SecondObstacleArch();
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+Adafruit_DCMotor *driveMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *turnMotor = AFMS.getMotor(2);
 
 void setup() {
   Serial.begin(9600);
@@ -18,6 +27,12 @@ void setup() {
   pinMode(FRONT_ECHO, INPUT);
   pinMode(LEFT_TRIG, OUTPUT);
   pinMode(LEFT_ECHO, INPUT);
+
+  AFMS.begin();
+
+  driveMotor->setSpeed(150);
+  turnMotor->setSpeed(150);
+  FIRST_OBSTACLE = true;
 }
 
 void loop() {
@@ -38,20 +53,58 @@ void loop() {
   Serial.print("WALL DISTANCE: ");
   Serial.println(Wall_Distance);
 
-  //If wall is more than MAX_WALL_DISTANCE move closer to wall
-  //Else if wall si less than MIN_WALL_DISTANCE move farther from wall
+  //No Obstacles
+  if(Front_Distance > MAX_FRONT_DIST){
+    driveMotor->setSpeed(150);
+    driveMotor->run(FORWARD);
+  }
 
-  //If obstacle is approaching
-    //If First Obstacle do FirstObstacleArch
-    //Else do SecondObstacleArch  
-  
+  //If wall is more than MAX_WALL_DISTANCE move closer to wall
+  if (Wall_Distance > MAX_DIST_TO_WALL) {
+    //turn left
+    turnMotor->run(BACKWARD);
+  }
+  //Else if wall is less than MIN_WALL_DISTANCE move farther from wall
+  else if (Wall_Distance < MIN_DIST_TO_WALL) {
+    //turn right
+    turnMotor->run(FORWARD);
+  }
+  //Else Release turn motor
+  else{
+    turnMotor->run(RELEASE);
+  }
+
+  //Getting close to an obstacle
+  if(Front_Distance < MAX_FRONT_DIST){
+    //SLOW DOWN and begin turning
+    driveMotor->setSpeed(50);
+    if(FIRST_OBSTACLE){
+      FirstObstacleArch();
+      FIRST_OBSTACLE = false;
+    }else{
+      SecondObstacleArch();
+    }
+  }
 }
 
+//Play with parameters of turning to determine best arches:
 void FirstObstacleArch(){
-  
+  for(int i = 0; i < 10; i++){
+      turnMotor->run(FORWARD);
+      delay(20);
+      turnMotor->run(RELEASE);
+      delay(10);
+    }
+    turnMotor->run(RELEASE);
 }
 
 void SecondObstacleArch(){
-  
+  for(int i = 0; i < 50; i++){
+      turnMotor->run(FORWARD);
+      delay(20);
+      turnMotor->run(RELEASE);
+      delay(10);
+    }
+    turnMotor->run(RELEASE);
 }
 
